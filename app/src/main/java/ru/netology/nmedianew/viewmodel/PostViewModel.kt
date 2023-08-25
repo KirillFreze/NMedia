9f8278906824f0f1a0cde9d8916dba8c1fb9f053
Пользователь <fkv88@mail.ru>
2023-08-25 14:48:03 +0700

    DataStorage

--- app/src/main/java/ru/netology/nmedianew/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedianew/viewmodel/PostViewModel.kt
@@ -1,10 +1,13 @@
 package ru.netology.nmedianew.viewmodel
 
+import android.app.Application
 import android.util.MutableDouble
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.netology.nmedianew.dto.Post
 import ru.netology.nmedianew.repository.PostRepository
+import ru.netology.nmedianew.repository.PostRepositoryFileImpl
 import ru.netology.nmedianew.repository.PostRepositoryInMemoryImpl
 
 private val empty = Post(
@@ -17,8 +20,8 @@ private val empty = Post(
     0,
     false
 )
-class PostViewModel: ViewModel() {
-    private  val repository: PostRepository = PostRepositoryInMemoryImpl()
+class PostViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: PostRepository = PostRepositoryFileImpl(application)
 
     val data = repository.getAll()
     val edited = MutableLiveData(empty)
